606cfbb9cf24b7344c6e0dd0ff42551c5a6a7a94
gallaugher <john.gallaugher@bc.edu>
2022-11-05 19:33:59 -0400

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,16 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//  Created by John Gallaugher on 11/5/22
+//  YouTube: YouTube.com/profgallaugher, Twitter: @gallaugher
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
